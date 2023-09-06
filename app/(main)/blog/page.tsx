26a23ce74009b4d33ac6d98cba65a18656bbe640
Cecil Tantay <cjbtantay@gmail.com>
2023-09-06 03:56:56 -0700

    iframe

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Iframe from "react-iframe";
 
 export default function Blog() {
-  return <main>this is the blog page</main>;
+  return (
+    <main className="mx-auto my-0 w-[100%] max-w-[1040px] bg-white px-[20px] py-[30px] md:mt-[60px]">
+      <Iframe
+        url="https://observablehq.com/embed/d0362de8e6708bab?cell=*"
+        className="h-[65vh] w-[100%] overflow-hidden border-none"
+        width="100%"
+        height="500"
+        frameBorder={0}
+      />
+    </main>
+  );
 }
