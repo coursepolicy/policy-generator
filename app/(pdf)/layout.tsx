d3e5f2a131ec51ee37fcae7c2caa2244d6bcf623
Cecil Tantay <cjbtantay@gmail.com>
2023-09-17 07:26:07 -0700

    remove gray pdf preview

--- app/(pdf)/layout.tsx
+++ app/(pdf)/layout.tsx
@@ -1,9 +1,63 @@
+import "../globals.css";
 import React from "react";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <body className="overscroll-y-contain bg-white">{children}</body>;
+const messinaSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/MessinaSans-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-BoldItalic.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-Book.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-LightItalic.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-RegularItalic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/MessinaSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-messina-sans",
+});
+
+export const metadata: Metadata = {
+  title: "CourcePolicy.AI",
+  description: "AI Policy Generation",
+};
+
+export default function PDFLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${messinaSans.variable} font-sans`}>
+      <body className="overscroll-y-contain bg-white">{children}</body>
+    </html>
+  );
 }
