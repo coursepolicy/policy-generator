5614e844bba247e4aa0ef9eddddadbed768f52c0
jh-tx <jhtx.javascript@gmail.com>
2024-04-27 15:02:06 -0500

    Adjusted the list of cities to be columns in mobile

--- app/(main)/home/WorldWideList.tsx
+++ app/(main)/home/WorldWideList.tsx
@@ -3,29 +3,29 @@ import Link from "next/link";
 
 export default function WorldWideList() {
   return (
-    <div className="mt-[78px] flex justify-between">
-      <ul className="list-none space-y-3 text-[21px] text-white">
+    <div className="mt-[78px] flex flex-col items-center lg:flex-row lg:justify-between">
+      <ul className="list-none space-y-3 pb-[20px] text-[21px] text-white lg:pb-0">
         <li>Boston,MA</li>
         <li>Palo Alto,CA</li>
         <li>New York,NY</li>
         <li>Dallas,TX</li>
         <li>Toronto,CA</li>
       </ul>
-      <div className="relative w-[15%]">
+      {/* <div className="relative w-[15%]">
         <Image src="/images/Line_14.svg" alt="line" objectFit="contain" fill />
-      </div>
-      <ul className="list-none space-y-3 text-[21px] text-white ">
+      </div> */}
+      <ul className="list-none space-y-3 border-y-2 pb-[20px] pt-[20px] text-[21px] text-white lg:border-x-2 lg:border-y-0 lg:pb-0 lg:pl-[45px] lg:pr-[45px] lg:pt-0 ">
         <li>Boston,MA</li>
         <li>Palo Alto,CA</li>
         <li>Austin,TX</li>
         <li>New York,NY</li>
         <li>New York,NY</li>
       </ul>
-      <div className="relative w-[15%]">
+      {/* <div className="relative w-[15%]">
         <Image src="/images/Line_14.svg" alt="line" objectFit="contain" fill />
-      </div>
+      </div> */}
       <div>
-        <ul className=" list-none space-y-3 text-[21px] text-white">
+        <ul className=" list-none space-y-3 pt-[20px] text-[21px] text-white lg:pt-0">
           <li>Boston,MA</li>
           <li>Palo Alto,CA</li>
           <li>Westwood,CA</li>
