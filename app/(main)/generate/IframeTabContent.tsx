1a69105f03187140b9b37a351e2de09044f71a61
cbtsao47 <cbtsao47@gmail.com>
2024-04-27 11:24:12 -0500

    prepopulate policy id in iframe form

--- app/(main)/generate/IframeTabContent.tsx
+++ app/(main)/generate/IframeTabContent.tsx
@@ -2,14 +2,15 @@ import { TabsContent } from "@/components/ui/tabs";
 import Iframe from "react-iframe";
 
 type Props = {
+  id: string;
   value: string;
 };
 
-export default function IframeTabContent({ value }: Props) {
+export default function IframeTabContent({ value, id }: Props) {
   return (
     <TabsContent value={value} className="w-full">
       <Iframe
-        url={process.env.SURVEY_URL as string}
+        url={`${process.env.SURVEY_URL}?CoursePolicyID=${id}`}
         className="h-[calc(100vh_-_160px)] w-full border-0 md:h-[calc(100vh_-_180px)] md:max-w-[600px]"
       />
     </TabsContent>
