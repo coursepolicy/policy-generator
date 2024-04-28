576745b6e6e60b0ae64024048b3f2bc20b2e07c2
jh-tx <jhtx.javascript@gmail.com>
2024-04-27 21:03:31 -0500

    Adjusted mobile based on feedback

--- app/hgse/home/LowerSection.tsx
+++ app/hgse/home/LowerSection.tsx
@@ -4,11 +4,7 @@ export default function LowerSection() {
       <p className="px-4 font-serif text-[32px] lg:px-0 lg:pt-[35px]">
         Ready to get started?
       </p>
-      <p
-        className="max-w-[740px] 
-           px-4 pt-[20px] text-left text-[16px]
-            leading-normal text-stone-500 lg:px-0 lg:pt-[35px] lg:text-left"
-      >
+      <p className="max-w-[740px] px-4 pt-[20px] text-left text-[16px] leading-normal text-stone-500 lg:px-0 lg:pt-[35px] lg:text-left">
         When we conducted a survey etc etc, we found that over 90% of course
         instructors think their students will use g enerative AI, regardless of
         what course policies say. Other insights. When we conducted a survey etc
