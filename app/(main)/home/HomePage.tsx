765484f353a8994aa31c22c9e503e91d78a408ba
jh-tx <jhtx.javascript@gmail.com>
2024-04-30 16:52:57 -0500

    hgse black banner addition

--- app/(main)/home/HomePage.tsx
+++ app/(main)/home/HomePage.tsx
@@ -16,21 +16,21 @@ export default function HomePage() {
   return (
     <>
       <main>
-        <div className="flex bg-black">
-          <div className="text-md mx-auto flex max-w-[1600px] py-3">
-            <p className="text-center text-[12px] text-white">
+        <div className="bg-black">
+          <div className="text-md mx-auto flex max-w-[1600px] flex-col items-center py-3 lg:flex-row lg:justify-center">
+            <p className="px-[5px] text-center text-[12px] text-white lg:px-0">
               To access CoursePolicy through Harvard Graduate School of
-              Education, click{" "}
-              <span className="text-[12px] text-coursePolicyLightGreen underline">
-                here
-              </span>
+              Education,&nbsp;
             </p>
-            <Link href="/"></Link>
-            <Image
-              className="ml-[5px]"
-              alt="right pointed arrow"
-              src={rightArrowGreen}
-            />
+            <Link className="flex items-center" href="/hgse">
+              <span className="text-[12px] text-white">click&nbsp;</span>
+              <p className="text-[12px] text-coursePolicyLightGreen underline">
+                here
+              </p>
+              <span className="ml-[5px]">
+                <Image alt="right pointed arrow" src={rightArrowGreen} />
+              </span>
+            </Link>
           </div>
         </div>
         <div className="bg-[#191f3c]">
