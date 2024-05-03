2e3c27984d61943c13ac31e7a155e0aa3c0aafe2
jh-tx <jhtx.javascript@gmail.com>
2024-05-02 23:02:36 -0500

    fixed that thing

--- app/hgse/home/TextHeader.tsx
+++ app/hgse/home/TextHeader.tsx
@@ -3,7 +3,7 @@ export default function TextHeader() {
     <h1 className="max-w-[650px] px-4 pt-[20px] text-left font-serif text-[42px] lg:ml-[150px] lg:px-0 lg:pb-[20px] lg:pt-[90px]">
       The step-by-step AI policy&nbsp;
       <span className="lg:block">
-        generator for Harvard Graduate School of Education's course&nbsp;
+        generator for Harvard Graduate School of Education&apos;s course&nbsp;
       </span>
       instructors.
     </h1>
