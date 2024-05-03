a08d1608ca42bbf5b49ef89efbb2e424c13f091b
jh-tx <jhtx.javascript@gmail.com>
2024-05-02 22:40:41 -0500

    removed useless spaces

--- app/hgse/home/TextHeader.tsx
+++ app/hgse/home/TextHeader.tsx
@@ -1,10 +1,10 @@
 export default function TextHeader() {
   return (
     <h1 className="max-w-[650px] px-4 pt-[20px] text-left font-serif text-[42px] lg:ml-[150px] lg:px-0 lg:pb-[20px] lg:pt-[90px]">
-      The step-by-step AI policy{" "}
+      The step-by-step AI policy
       <span className="lg:block">
         generator for Harvard Graduate School of Education's course
-      </span>{" "}
+      </span>
       instructors.
     </h1>
   );
