173af276a1c147419b2709b699d728978c8a2178
jh-tx <jhtx.javascript@gmail.com>
2024-05-02 23:00:20 -0500

    Fixed spacing and step interval

--- app/hgse/home/HarvardLogoSection.tsx
+++ app/hgse/home/HarvardLogoSection.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function HarvardLogoSection() {
   return (
     <div className="flex h-full flex-col items-center lg:ml-[17px] lg:mt-[22px] lg:flex-row">
-      <h1 className="text-[15px] lg:pr-[14px] lg:text-[19px]">
+      <h2 className="text-[15px] lg:pr-[14px] lg:text-[19px]">
         in collaboration with:
-      </h1>
+      </h2>
       <Image
         className="lg:mr-[18px]"
         src="/images/hgse/HGSE_Logo_H.svg"
--- app/hgse/home/TextHeader.tsx
+++ app/hgse/home/TextHeader.tsx
@@ -1,9 +1,9 @@
 export default function TextHeader() {
   return (
     <h1 className="max-w-[650px] px-4 pt-[20px] text-left font-serif text-[42px] lg:ml-[150px] lg:px-0 lg:pb-[20px] lg:pt-[90px]">
-      The step-by-step AI policy
+      The step-by-step AI policy&nbsp;
       <span className="lg:block">
-        generator for Harvard Graduate School of Education&apos;s course
+        generator for Harvard Graduate School of Education's course&nbsp;
       </span>
       instructors.
     </h1>
