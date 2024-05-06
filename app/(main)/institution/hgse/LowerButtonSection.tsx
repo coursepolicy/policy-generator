2f11d8d7a1b9496d00910d528014da85022aa436
jh-tx <jhtx.javascript@gmail.com>
2024-05-06 12:25:23 -0500

    Adjusted several elements based on feedback

--- app/(main)/institution/hgse/LowerButtonSection.tsx
+++ app/(main)/institution/hgse/LowerButtonSection.tsx
@@ -5,11 +5,11 @@ import rightArrowWhite from "@/public/images/right-arrow-white.svg";
 
 export default function LowerButtonSection() {
   return (
-    <div className="lg:mt-[35px]">
+    <div className="lg:mt-[25px]">
       <div className="mt-[50px] flex flex-col px-4 lg:mt-[0px] lg:flex-row lg:px-0">
         <Button
           asChild
-          className="mb-6 h-[40px] w-[195px] items-center justify-center gap-2.5 rounded-[100px] bg-[#A41D30] text-sm font-semibold leading-[25px] text-white hover:bg-coursePolicyHoverGreen lg:mb-[0px] lg:mr-4"
+          className="mb-6 h-[40px] w-[195px] items-center justify-center gap-2.5 rounded-[100px] bg-[#A41D30] text-sm leading-[25px] text-white hover:bg-coursePolicyHoverGreen lg:mb-[0px] lg:mr-4"
         >
           <Link href="/generate/hgse">
             Create an AI Policy{" "}
@@ -19,7 +19,7 @@ export default function LowerButtonSection() {
         <Button
           asChild
           variant={"outline"}
-          className="mb-[50px] h-[40px] w-[195px] items-center justify-center gap-2.5 rounded-[100px] border border-[#4A558E] bg-transparent text-right text-sm leading-[25px] text-[#4A558E] hover:text-[#4A558E] lg:mb-0 lg:ml-[5px] lg:w-auto"
+          className="mb-[50px] h-[40px] w-[195px] items-center justify-center gap-2.5 rounded-[100px] border border-[#4A558E] bg-transparent px-6 text-right text-sm leading-[25px] text-[#4A558E] hover:text-[#4A558E] lg:mb-0 lg:ml-[5px] lg:w-auto"
         >
           <Link href="/policy/sample/hgse">View sample policy</Link>
         </Button>
--- app/(main)/institution/hgse/TextParagraph.tsx
+++ app/(main)/institution/hgse/TextParagraph.tsx
@@ -1,14 +1,14 @@
 export default function TextParagraph() {
   return (
-    <div className="flex md:mt-[7px]">
-      <p className="max-w-[640px] px-4 pt-[20px] text-left text-[16px] leading-normal text-stone-500">
+    <div className="flex pr-[48px] lg:mr-[80px] lg:pr-0 lg:pt-[10px]">
+      <p className="max-w-[800px] px-4 pt-[20px] text-left text-[16px] leading-normal text-stone-500 lg:px-0">
         With the rise of tools like ChatGPT in student learning, how prepared
         are educators to set clear guidelines? We surveyed professors from 14
         major U.S. universities, gathering insights from 207 responses. Our
         findings reveal varied awareness and plans around AI policy
         implementation. Explore our detailed survey or check out sample policies
         for deeper insights.{" "}
-        <span className="block pt-[10px] lg:pt-[20px]">
+        <span className="block pt-[40px]">
           * Please read our survey report to see the full context
         </span>
       </p>
