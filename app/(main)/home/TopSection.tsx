809d9a5d39d0f82ce341d3af6add1eff102b3848
jh-tx <jhtx.javascript@gmail.com>
2024-05-16 19:26:19 -0500

    adjusted verbiage and removed a space

--- app/(main)/home/TopSection.tsx
+++ app/(main)/home/TopSection.tsx
@@ -6,7 +6,7 @@ import CopyTextHeader from "./CopyTextHeader";
 export default function TopSection() {
   return (
     <section className="bg-[#191f3c]">
-      <div className="mx-auto flex max-w-[1600px] flex-col items-center text-left lg:flex-row lg:justify-center 2xl:justify-between">
+      <div className="mx-auto flex max-w-[1600px] flex-col items-center text-left lg:flex-row lg:justify-center">
         <div className="px-4 lg:ml-[147px] lg:px-0">
           <CopyTextHeader />
           <div className="mt-[20px] flex items-center lg:justify-between">
