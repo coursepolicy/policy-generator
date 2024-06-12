425bd03b7773edac4b0d29c0266faa368f8e312f
jh-tx <jhtx.javascript@gmail.com>
2024-06-11 20:32:55 -0500

    Updated remaining font on HGSE landing page

--- app/(main)/institution/hgse/HGSEMidsection.tsx
+++ app/(main)/institution/hgse/HGSEMidsection.tsx
@@ -4,7 +4,7 @@ export default function HGSEMidsection() {
     <div className="bg-[#FFFFFF]">
       <section className="mx-auto flex max-w-[1600px] items-center justify-center px-4 lg:pb-[72px]">
         <div className="flex flex-col pt-[100px] text-center lg:ml-[100px] lg:text-left">
-          <h2 className="font-serif text-[32px]">
+          <h2 className="font-crimson text-[32px]">
             Why do I need an AI policy?
           </h2>
           <p className="max-w-[800px] px-4 pt-[20px] text-center text-[16px] leading-normal text-stone-500 lg:px-0  lg:pt-[30px] lg:text-left">
@@ -14,7 +14,7 @@ export default function HGSEMidsection() {
           </p>
           <div className="flex flex-col items-center justify-center p-4 lg:flex-row lg:justify-between lg:p-0 lg:pt-[45px]">
             <div className="flex w-[100%] max-w-[450px] flex-col border-t-[3px] border-[#A41D30] py-8 text-center lg:mr-[35px] lg:max-w-[240px] lg:pb-[32px] lg:pt-[23px] lg:text-left">
-              <h3 className="text-center font-serif text-[24px] lg:text-left">
+              <h3 className="text-center font-crimson text-[24px] lg:text-left">
                 Empower student learning
               </h3>
               <p className="pt-[20px] text-center text-[16px] text-stone-500 lg:text-left">
@@ -23,7 +23,7 @@ export default function HGSEMidsection() {
               </p>
             </div>
             <div className="flex w-[100%] max-w-[450px] flex-col border-t-[3px] border-[#A41D30] py-8 text-center lg:mr-[35px] lg:max-w-[240px] lg:pb-[32px] lg:pt-[23px] lg:text-left">
-              <h3 className="text-center font-serif text-[24px] lg:text-left">
+              <h3 className="text-center font-crimson text-[24px] lg:text-left">
                 Facilitate policy communication
               </h3>
               <p className="pt-[20px] text-center text-[16px] text-stone-500 lg:text-left">
@@ -32,7 +32,7 @@ export default function HGSEMidsection() {
               </p>
             </div>
             <div className="flex w-[100%] max-w-[450px] flex-col border-t-[3px] border-[#A41D30] py-8 text-center lg:mr-[35px] lg:max-w-[240px] lg:pb-[32px] lg:pt-[23px] lg:text-left">
-              <h3 className="text-center font-serif text-[24px] lg:text-left">
+              <h3 className="text-center font-crimson text-[24px] lg:text-left">
                 Encourage constructive discourse
               </h3>
               <p className="pt-[20px] text-center text-[16px] text-stone-500 lg:mb-[24px] lg:text-left">
