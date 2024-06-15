23af653af585ff355b2fd5ac964e96d94823fb3b
jh-tx <jhtx.javascript@gmail.com>
2024-06-15 13:33:58 -0500

    added X button to toolip

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,15 +1,27 @@
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Tooltip() {
-  return (
-    <div className="flex min-h-[59px] w-[100%] items-center justify-start rounded-[3px] bg-[#3284ff] pl-[20px] md:mb-[25px] md:ml-[20px] md:mt-[50px] md:w-[96%]">
-      <p>
+  const [hidden, setHidden] = useState(false);
+
+  const removeTooltip = () => {
+    setHidden(true);
+  };
+
+  return hidden ? null : (
+    <div className="flex min-h-[59px] w-[100%] items-center justify-between rounded-[3px] bg-[#3284ff] pl-[20px] md:mb-[25px] md:ml-[20px] md:mt-[50px] md:w-[96%]">
+      <div className="flex flex-col">
         <span className="text-xs font-bold leading-normal text-white">
           INSTRUCTIONS
         </span>
-        <span className="flex text-xs font-normal leading-normal text-white">
+        <span className="text-xs font-normal leading-normal text-white">
           To make modifications to your AI policy, try clicking on a section to
           start editing the content.
         </span>
-      </p>
+      </div>
+      <button className="mb-4 p-2 text-white" onClick={removeTooltip}>
+        <Image src="/images/x.svg" alt="x icon" width={20} height={20} />
+      </button>
     </div>
   );
 }
