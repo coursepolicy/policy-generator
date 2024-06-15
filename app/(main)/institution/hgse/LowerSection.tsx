8ef1389809ef0123a95f5c38f16f2fdcfe19954e
dev-hausen <142753555+jh-tx@users.noreply.github.com>
2024-06-15 12:16:00 -0500

    Font fixes (#72)
    * fixed font issue

--- app/(main)/institution/hgse/LowerSection.tsx
+++ app/(main)/institution/hgse/LowerSection.tsx
@@ -4,7 +4,7 @@ export default function LowerSection() {
     <section className="flex items-center justify-center bg-[#F6F6F9] px-4 lg:px-0 lg:pb-[28px]">
       <div className="flex max-w-[1600px] flex-col lg:pb-[60px] lg:pl-[20px] lg:pt-[70px] lg:text-left">
         <div className="flex flex-col pt-[50px] text-left md:mt-[7px] lg:pt-0">
-          <h2 className="px-4 font-serif text-[30px] lg:px-0">
+          <h2 className="px-4 font-crimson text-[30px] lg:px-0">
             Ready to get started?
           </h2>
           <p className="max-w-[800px] px-4 pt-[30px] text-left text-[16px] leading-normal text-stone-500 lg:px-0 lg:text-left">
